c85e0d925f25edb25e12ae508fe943a64947197c
RyanKendrick <rpkendrick@gmail.com>
2020-01-11 13:41:41 -0500

    queried the comments API

--- src/views/Single.vue
+++ src/views/Single.vue
@@ -5,8 +5,43 @@
 </template>
 
 <script>
+  import axios from "axios";
   export default {
     name: "Single",
+    data: function() {
+      return {
+        story: {},
+        comments: []
+      };
+    },
+    created: function() {
+      axios
+        .get(
+          "https://hacker-news.firebaseio.com/v0/item/" +
+            this.$route.params.id +
+            ".json"
+        )
+        .then(res => {
+          this.story = res.data;
+          this.story.comments = [];
+          this.story.kids.forEach(id => {
+            axios
+              .get("https://hacker-news.firebaseio.com/v0/item/" + id + ".json")
+              .then(res => {
+                this.$nextTick(() => {
+                  (res.data);
+                  this.comments.push(res.data);
+                });
+              })
+              .catch(err => {
+                (err);
+              });
+          });
+        })
+        .catch(err => {
+          (err);
+        });
+    }
   };
 </script>
 <style scoped></style>
